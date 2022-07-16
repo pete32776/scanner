06b1de7c7beffb929c12df60741baf78c45a373a
Pete Wright <petewright32038@gmail.com>
2022-07-16 16:28:41 -0500

    cleaned up code

--- network_scanner.py
+++ network_scanner.py
@@ -8,20 +8,16 @@ import argparse
 #parses arguments provided by user at the command line
 def parse_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--ip_range", dest="ip", help="ip range to scan"),
-    parser.add_argument("-b", "--beginning", dest="beginning_address", help="first address in range to scan")
-    parser.add_argument("-e", "--ending", dest="ending_address", help="last address in range to scan")
+    parser.add_argument("-t", "--target", dest="ip", help="ip range to scan")
     return parser.parse_args()
 
 
+#scan ip or ip range and returns dictionary of ip addresses and associated mac addresses
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    #arp_request.show()
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    #broadcast.show()
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    #print(answered_list.summary())
     client_list = []
     for item in answered_list:
         client_dict = {"ip":item[1].psrc,"mac":item[1].hwsrc}
@@ -29,7 +25,7 @@ def scan(ip):
     return client_list
 
 
-
+#print client list ip/mac
 def print_client_list(client_list):
     print("*"*75)
     print("IP\t\t\tMac Address")
@@ -38,12 +34,10 @@ def print_client_list(client_list):
         print(item["ip"],"\t\t", end="")
         print(item["mac"])
         print("....................................")
-    #arp_request_broadcast.show()
-    #scapy.ls(scapy.Ether)
-    #print(arp_request.summary())
-    #scapy.ls(scapy.ARP())
+
+
+#main
 args = parse_input()
-print(args.ip)
 scan_result = scan(args.ip)
 print_client_list(scan_result)
 
